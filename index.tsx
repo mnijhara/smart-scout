16b02cd1d9c31ba3c448956fe09ef84e87b2162e
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-13 22:07:37 +0530

    feat: make hiring workflow the Smart Scout home

--- index.tsx
+++ index.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import RecruitingOS from './components/RecruitingOS';
 import JobIntelligence from './components/JobIntelligence';
 
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  const params = new URLSearchParams(window.location.search);
-  const path = window.location.pathname.replace(/\/$/, '') || '/';
-  const feature = params.get('feature');
-  root.render(
-    <React.StrictMode>
-      {path === '/old' ? <App /> : feature === 'job' ? <JobIntelligence onBack={() => window.location.assign('/')} /> : <RecruitingOS />}
-    </React.StrictMode>
-  );
-}
+const container=document.getElementById('root');
+if(container){const root=createRoot(container);const path=window.location.pathname.replace(/\/$/,'')||'/';root.render(<React.StrictMode>{path==='/old'?<App/>:<JobIntelligence/>}</React.StrictMode>);}
