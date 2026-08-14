ffbccdf70153097a8baf60262d10a92ff606de55
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-14 21:45:08 +0530

    feat: route hire page through complete lifecycle UI

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import JobIntelligenceConnected from './components/JobIntelligenceConnected';
+import HiringLifecycleConnected from './components/HiringLifecycleConnected';
 import LandingPage from './components/LandingPage';
 const container=document.getElementById('root');
-if(container){const root=createRoot(container);const path=window.location.pathname.replace(/\/$/,'')||'/';const view=path==='/old'?<App/>:path==='/hire'?<JobIntelligenceConnected onBack={()=>window.location.assign('/')}/>:<LandingPage onUseOwn={()=>window.location.assign('/hire')}/>;root.render(<React.StrictMode>{view}</React.StrictMode>)}
+if(container){const root=createRoot(container);const path=window.location.pathname.replace(/\/$/,'')||'/';const view=path==='/old'?<App/>:path==='/hire'?<HiringLifecycleConnected onBack={()=>window.location.assign('/')}/>:<LandingPage onUseOwn={()=>window.location.assign('/hire')}/>;root.render(<React.StrictMode>{view}</React.StrictMode>)}
