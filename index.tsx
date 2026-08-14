bb8ca502ceb0f61ef77f4258b04de0a803a4b8ae
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-14 20:46:27 +0530

    feat: route hire flow through connected hiring UI

--- index.tsx
+++ index.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import JobIntelligence from './components/JobIntelligence';
+import JobIntelligenceConnected from './components/JobIntelligenceConnected';
 import LandingPage from './components/LandingPage';
-
 const container=document.getElementById('root');
-if(container){
-  const root=createRoot(container);
-  const path=window.location.pathname.replace(/\/$/,'')||'/';
-  const view=path==='/old'?<App/>:path==='/hire'?<JobIntelligence/>:<LandingPage onUseOwn={()=>{window.location.assign('/hire')}}/>;
-  root.render(<React.StrictMode>{view}</React.StrictMode>);
-}
+if(container){const root=createRoot(container);const path=window.location.pathname.replace(/\/$/,'')||'/';const view=path==='/old'?<App/>:path==='/hire'?<JobIntelligenceConnected onBack={()=>window.location.assign('/')}/>:<LandingPage onUseOwn={()=>window.location.assign('/hire')}/>;root.render(<React.StrictMode>{view}</React.StrictMode>)}
