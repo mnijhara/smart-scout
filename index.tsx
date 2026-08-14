fa79e3bac9a0845c8654c8363c375da92bc13b7d
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-14 23:27:51 +0530

    feat: route hire page through BYOK lifecycle

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import HiringLifecycleConnected from './components/HiringLifecycleConnected';
+import HiringLifecycleV2 from './components/HiringLifecycleV2';
 import LandingPage from './components/LandingPage';
 const container=document.getElementById('root');
-if(container){const root=createRoot(container);const path=window.location.pathname.replace(/\/$/,'')||'/';const view=path==='/old'?<App/>:path==='/hire'?<HiringLifecycleConnected onBack={()=>window.location.assign('/')}/>:<LandingPage onUseOwn={()=>window.location.assign('/hire')}/>;root.render(<React.StrictMode>{view}</React.StrictMode>)}
+if(container){const root=createRoot(container);const path=window.location.pathname.replace(/\/$/,'')||'/';const view=path==='/old'?<App/>:path==='/hire'?<HiringLifecycleV2 onBack={()=>window.location.assign('/')}/>:<LandingPage onUseOwn={()=>window.location.assign('/hire')}/>;root.render(<React.StrictMode>{view}</React.StrictMode>)}
