9e260f644365fb3c8688ceb5bdd5218f32e7d2a8
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-14 23:29:22 +0530

    ui: load compact hiring workspace polish

--- index.tsx
+++ index.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import HiringLifecycleV2 from './components/HiringLifecycleV2';
 import LandingPage from './components/LandingPage';
+import './hire-ui.css';
 const container=document.getElementById('root');
 if(container){const root=createRoot(container);const path=window.location.pathname.replace(/\/$/,'')||'/';const view=path==='/old'?<App/>:path==='/hire'?<HiringLifecycleV2 onBack={()=>window.location.assign('/')}/>:<LandingPage onUseOwn={()=>window.location.assign('/hire')}/>;root.render(<React.StrictMode>{view}</React.StrictMode>)}
