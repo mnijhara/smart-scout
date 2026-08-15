ee2b8f45262c911d6e7d095128e91e9d57562f5f
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-15 17:34:31 +0530

    feat: switch public homepage to dense product experience

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import HiringLifecycleV2 from './components/HiringLifecycleV2';
-import LandingPage from './components/LandingPage';
+import LandingPageV2 from './components/LandingPageV2';
 import AuthGate from './components/AuthGate';
 import ByokWidget from './components/ByokWidget';
 import './hire-ui.css';
@@ -12,7 +12,7 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   const path = window.location.pathname.replace(/\/$/, '') || '/';
-  const publicHome = <><LandingPage onUseOwn={() => window.location.assign('/hire')} /><ByokWidget /></>;
+  const publicHome = <><LandingPageV2 onUseOwn={() => window.location.assign('/hire')} /><ByokWidget /></>;
   const lifecycle = <AuthGate><HiringLifecycleV2 onBack={() => window.location.assign('/')} /></AuthGate>;
 
   const view =
