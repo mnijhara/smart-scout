129855324232ca6e794505d849616681d4702ac5
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-16 18:42:27 +0530

    fix: support JSX key attributes in lifecycle components

--- types/react-jsx.d.ts
+++ types/react-jsx.d.ts
@@ -1,4 +1,13 @@
+import 'react';
 import 'react/jsx-runtime';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      key?: string | number | bigint | null | undefined;
+    }
+  }
+}
 
 declare module 'react/jsx-runtime' {
   namespace JSX {
