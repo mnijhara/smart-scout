b09e892bda064d15835d80f60991a86925ea75dd
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-16 17:51:02 +0530

    fix: route verified lifecycle component

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import HiringLifecycleV3 from './components/HiringLifecycleV3';
+import HiringLifecycleV4 from './components/HiringLifecycleV4';
 import JDImportBridge from './components/JDImportBridge';
 import LandingPageFinal from './components/LandingPageFinal';
 import MagicHiringDemoV3 from './components/MagicHiringDemoV3';
@@ -13,7 +13,7 @@ if (container) {
   const root = createRoot(container);
   const path = window.location.pathname.replace(/\/$/, '') || '/';
   const publicHome = <><LandingPageFinal onStart={() => window.location.assign('/hire')} /><MagicHiringDemoV3 onUseOwn={() => window.location.assign('/hire')} /></>;
-  const lifecycle = <AuthGate><><JDImportBridge /><HiringLifecycleV3 onBack={() => window.location.assign('/')} /></></AuthGate>;
+  const lifecycle = <AuthGate><><JDImportBridge /><HiringLifecycleV4 onBack={() => window.location.assign('/')} /></></AuthGate>;
   const view = path === '/old' ? <App /> : path === '/hire' ? lifecycle : publicHome;
   root.render(<React.StrictMode>{view}</React.StrictMode>);
 }
