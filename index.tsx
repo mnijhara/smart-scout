a6f8c6a1637f049506fe4d2d4f1cb94beb6ba8a4
miteshnijhara-code <mitesh.nijhara@gmail.com>
2026-08-16 17:49:08 +0530

    feat: route production hiring workspace through approval-driven lifecycle

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import HiringLifecycleV2 from './components/HiringLifecycleV2';
+import HiringLifecycleV3 from './components/HiringLifecycleV3';
 import JDImportBridge from './components/JDImportBridge';
 import LandingPageFinal from './components/LandingPageFinal';
 import MagicHiringDemoV3 from './components/MagicHiringDemoV3';
@@ -9,27 +9,11 @@ import AuthGate from './components/AuthGate';
 import './hire-ui.css';
 
 const container = document.getElementById('root');
-
 if (container) {
   const root = createRoot(container);
   const path = window.location.pathname.replace(/\/$/, '') || '/';
-
-  const publicHome = (
-    <>
-      <LandingPageFinal onStart={() => window.location.assign('/hire')} />
-      <MagicHiringDemoV3 onUseOwn={() => window.location.assign('/hire')} />
-    </>
-  );
-
-  const lifecycle = (
-    <AuthGate>
-      <>
-        <JDImportBridge />
-        <HiringLifecycleV2 onBack={() => window.location.assign('/')} />
-      </>
-    </AuthGate>
-  );
-
+  const publicHome = <><LandingPageFinal onStart={() => window.location.assign('/hire')} /><MagicHiringDemoV3 onUseOwn={() => window.location.assign('/hire')} /></>;
+  const lifecycle = <AuthGate><><JDImportBridge /><HiringLifecycleV3 onBack={() => window.location.assign('/')} /></></AuthGate>;
   const view = path === '/old' ? <App /> : path === '/hire' ? lifecycle : publicHome;
   root.render(<React.StrictMode>{view}</React.StrictMode>);
 }
